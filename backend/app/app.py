c1d994986d448690cfc87b6b9b0484062ae33a00
Alyona <noodlenickels@gmail.com>
2024-04-25 22:29:34 +0300

    redis done

---
+++ backend/app/app.py
@@ -0,0 +1,79 @@
+# from flask import Flask, jsonify, render_template
+# from database import CarMaintenanceService
+#
+# app = Flask(__name__)
+# service = CarMaintenanceService()
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+# @app.route('/total_revenue_per_month')
+# def total_revenue_per_month():
+#     data = service.get_total_revenue_per_month()
+#     return jsonify(data)
+#
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask, jsonify, render_template, after_this_request
+from flask_redis import FlaskRedis
+import psycopg2
+
+
+app = Flask(__name__)
+# Configure your Redis URI
+app.config['REDIS_URL'] = "redis://localhost:6379/0"
+
+# Initialize the Redis client
+redis_client = FlaskRedis(app)
+@app.route('/pics', methods=['GET'])
+def hello():
+    @after_this_request
+    def add_header(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    data = redis_client.lrange('pics', 0, -1)
+    return [d.decode("utf-8") for d in data]
+
+@app.route('/getbd')
+def index():
+    # Connect to the database
+        conn = psycopg2.connect(database="postgres",
+                                user="noodlenickels",
+                                password="welcome29I",
+                                host="127.0.0.1", port="5432")
+        print("Database connected successfully")
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # Select all products from the table
+        cur.execute('''SELECT * FROM services''')
+
+        # Fetch the data
+        data = cur.fetchall()
+
+
+        # close the cursor and connection
+        cur.close()
+        conn.close()
+
+        @after_this_request
+            def add_header(response):
+                response.headers.add('Access-Control-Allow-Origin', '*')
+                return response
+
+        return data
+
+def total_revenue_per_month():
+    data = redis_client.get('pics')
+    return jsonify([u.to_json() for u in data])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ frontend/src/App.vue
@@ -0,0 +1,105 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const pictures = ref();
+
+onMounted(() => {
+  fetch('http://127.0.0.1:5000/pics')
+      .then(response => response.json())
+      .then(data => {
+        pictures.value = data;
+      });
+
+  fetch('http://127.0.0.1:5000/getdb')
+      .then(response => response.json())
+      .then(data => {
+        pictures.value = data;
+      })})
+
+
+
+</script>
+<template>
+
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+    <h2 class="mt-4">Car maintenance service</h2>
+    <p>Описать, как это вообще все на сайте работает</p>
+
+    <div class="d-flex mt-5 ms-5 me-5">
+      <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+        <ul class="nav nav-pills flex-column mb-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Orders
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Products
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link link-dark">
+              Customers
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="mb-3 w-100">
+        <div class="row mb-5 w-100">
+          <div class="col-4 themed-grid-col">Название услуги</div>
+          <div class="col-4 themed-grid-col">Тип услуги</div>
+          <div class="col-4 themed-grid-col">Стоимость</div>
+        </div>
+        <div class="row mb-5 w-100">
+          <div class="col-4 themed-grid-col">Клининг</div>
+          <div class="col-4 themed-grid-col">Внутренний</div>
+          <div class="col-4 themed-grid-col">100 рублей</div>
+        </div>
+        <div class="row mb-5 w-100">
+          <div class="col-4 themed-grid-col">Название услуги</div>
+          <div class="col-4 themed-grid-col">Тип услуги</div>
+          <div class="col-4 themed-grid-col">Стоимость</div>
+        </div>
+        <div class="row mb-5 w-100">
+          <div class="col-4 themed-grid-col">Название услуги</div>
+          <div class="col-4 themed-grid-col">Тип услуги</div>
+          <div class="col-4 themed-grid-col">Стоимость</div>
+        </div>
+        <div class="row mb-5 w-100">
+          <div class="col-4 themed-grid-col">Название услуги</div>
+          <div class="col-4 themed-grid-col">Тип услуги</div>
+          <div class="col-4 themed-grid-col">Стоимость</div>
+        </div>
+      </div>
+    </div>
+  <img v-for="(el, index) in pictures" :key="index" :src="el">
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
